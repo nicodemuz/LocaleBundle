43789bde615305a840e3ba9099d127582c84f6c6
Philipp Mohrenweiser <phiamo@googlemail.com>
2024-11-04 00:05:52 +0100

    more

--- src/Validator/MetaValidator.php
+++ src/Validator/MetaValidator.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the LuneticsLocaleBundle package.
  *
@@ -7,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that is distributed with this source code.
  */
+
 namespace Lunetics\LocaleBundle\Validator;
 
 use Symfony\Component\Validator\Validator\ValidatorInterface;
