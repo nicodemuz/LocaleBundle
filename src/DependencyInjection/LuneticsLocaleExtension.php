4370411fba39abb0fdae524c90c01b04068d9812
Philipp Mohrenweiser <phiamo@googlemail.com>
2025-01-05 01:23:39 +0100

    remove deprecations

--- src/DependencyInjection/LuneticsLocaleExtension.php
+++ src/DependencyInjection/LuneticsLocaleExtension.php
@@ -13,7 +13,7 @@ namespace Lunetics\LocaleBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Yaml\Parser as YamlParser;
 
